ed45c8f2eec2811b0db7cb02cbce4ebc93c4e68f
Daniel Sousa <daniel.s@crowdbotics.com>
2024-01-18 16:34:08 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "still-mode-3",
+  url: "https://still-mode-3.botics.co",
+  api: "https://still-mode-3.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
